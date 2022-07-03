2b084274d9c5699f6562663d0ac5f88da735a0cf
unknown <jrhagelin@gmail.com>
2022-07-03 12:50:18 +0200

    ImGUI base implemented

--- include/gui.h
+++ include/gui.h
@@ -4,23 +4,71 @@
 #include <imgui_impl_opengl3.h>
 
 class GUI {
-  public:
-  void init (GLFWwindow* window, const std::string& glsl_version) {
+private:
+  
+  auto set_solidness(float transparency) -> void {
+    ImGuiStyle& style = ImGui::GetStyle();
+    const auto before = style.Colors[ImGuiCol_WindowBg];
+    const auto after = ImVec4(before.x, before.y, before.z, transparency);
+    style.Colors[ImGuiCol_WindowBg] = after;
+  }
+  
+  auto transparent() -> void {
+    set_solidness(0.50f);
+  }
+  
+  auto solid() -> void {
+    set_solidness(1.00f);
+  }
+
+  auto basic_colors() -> void {
+    ImGuiStyle& style = ImGui::GetStyle();
+    // Set background colors of window and child to 3x 16 and 3x 24 respectively
+    style.Colors[ImGuiCol_WindowBg] = ImColor(16,16,16);
+    style.Colors[ImGuiCol_ChildBg] = ImColor(24,24,24);
+  }
+
+public:
+  auto init (GLFWwindow* window, const std::string& glsl_version) -> void {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version.c_str());
     ImGui::StyleColorsDark();
+    basic_colors();
   };
 
-  void new_frame() {
+  auto new_frame() -> void {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
   }
+  
+  
+  auto update () -> void {
+    
+    {
+      ImGui::Begin("Helloooo", 0, ImGuiWindowFlags_NoTitleBar);
+      ImGui::BeginChild("scrolling", ImVec2(0, 0), true, ImGuiWindowFlags_HorizontalScrollbar);
+      for (int n = 0; n < 100; n++)
+        ImGui::Text("%04d: Some text", n);
+      ImGui::EndChild();
+      ImGui::End();
+    }
+    
+  };
 
-  void update () {};
-  void render() {};
-  void shutdown() {};
+  auto render() -> void {
+    // Render dear imgui into screen
+    ImGui::Render();
+	  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  };
+
+  auto shutdown() -> void {
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+  };
 };
--- main.cpp
+++ main.cpp
@@ -16,44 +16,39 @@ void log(const char* msg) {
 }
 
 auto main(int argc, char** argv) -> int{
-    // Initialize glfw3
-    log("Initializing glfw3");
-    if (!glfwInit())
-        return -1;
-    
-    // Create a windowed mode window and its OpenGL context
-    // GL 3.0 + GLSL 130. https://www.khronos.org/opengl/wiki/Core_Language_(GLSL)
-    const std::string glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Setup window
+	if (!glfwInit())
+		return 1;
 
-    log("Creating window");
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Imgui", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-    // Make the window's context current
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    log("Initializing glad");
-    // glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Unable to context to OpenGL\n";
-        return -1;
-    }
-    log("Initializing imgui");
-    GUI gui;
-    gui.init(window, glsl_version);
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-        gui.new_frame();
-        gui.update();
-        gui.render();
-        glfwSwapBuffers(window);
-    }
-    log("Shutting down");
-    gui.shutdown();
+	// GL 3.0 + GLSL 130
+	const char *glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	// Create window with graphics context
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "Logbook", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  // tie window context to glad's opengl funcs
+		throw("Unable to context to OpenGL");
+
+	int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+	GUI gui;
+	gui.init(window, glsl_version);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		gui.new_frame();
+		gui.update();
+		gui.render();
+		glfwSwapBuffers(window);
+	}
+	gui.shutdown();
     return 0;
 }
